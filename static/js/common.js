41e1da425889fb4a297a2ef8e949440af341ac44
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-15 03:16:34 +0400

    delete news

--- static/js/common.js
+++ static/js/common.js
@@ -13,4 +13,12 @@ $(function(){
 
     var r = Raphael('graphics', 300, 300);
     r.piechart(50, 50, 50, [76, 70, 67, 71, 69]);
+
+    var fin = function () {
+            this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
+        },
+        fout = function () {
+            this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+        };
+    r.barchart(60, 50, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10]]).hover(fin, fout);
 });
