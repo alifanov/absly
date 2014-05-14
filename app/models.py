6c4e79380193d738a1187fd5e1ff79757a2c7c15
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-14 17:05:31 +0400

    delete news

--- app/models.py
+++ app/models.py
@@ -57,8 +57,8 @@ class News(models.Model):
             if not block: break
             lf.write(block)
         lf.flush()
-        self.photo.save(fname, files.File(lf), save=True)
-        self.photo = get_thumbnail(self.photo, '100x100', crop='center', quality=99)
+        thumbnail = get_thumbnail(files.File(lf), '100x100', crop='center', quality=99)
+        self.photo.save(fname, thumbnail, save=True)
 
     def __unicode__(self):
         return self.title
