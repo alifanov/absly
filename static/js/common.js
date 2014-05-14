a44c601846f74a690cff13f987acd39527237235
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-14 23:45:59 +0400

    delete news

--- static/js/common.js
+++ static/js/common.js
@@ -11,6 +11,8 @@ $(function(){
         return false;
     });
 
-    var r = Raphael('graphics');
-    r.piechart(10, 10, 100, [10, 20, 50, 20, 30]);
-});
+//    var r = Raphael();
+    var r = Raphael('graphics', 640, 480);
+// Creates pie chart at with center at 320, 200,
+// radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
+    r.piechart(320, 240, 100, [55, 20, 13, 32, 5, 1, 2]);});
