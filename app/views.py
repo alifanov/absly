1958d3ef6751ebe79818f167459c772d3d84f3f9
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-16 16:38:43 +0400

    add registration

--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ class MetricsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(MetricsView, self).get_context_data(**kwargs)
-        ctx['active'] = 'steps'
+        ctx['active'] = 'metrics'
         return ctx
 
 class StepsView(TemplateView):
