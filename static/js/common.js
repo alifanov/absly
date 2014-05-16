76e44ac41a1efdf10396a465a97d9c5d83b8515d
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-16 14:55:44 +0400

    add registration

--- static/js/common.js
+++ static/js/common.js
@@ -11,20 +11,26 @@ $(function(){
         return false;
     });
 
-    var r = Raphael('graphics', 400, 250);
-    r.piechart(70, 50, 50, [76, 70, 67, 71, 69]);
-    r.piechart(70, 170, 50, [70, 67, 71, 69, 76, 76, 76]);
+    if($("#graphics").length)
+    {
+        var r = Raphael('graphics', 400, 250);
+        r.piechart(70, 50, 50, [76, 70, 67, 71, 69]);
+        r.piechart(70, 170, 50, [70, 67, 71, 69, 76, 76, 76]);
 
-    var fin = function () {
-            this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
-        },
-        fout = function () {
-            this.flag.animate({opacity: 0}, 300, function () {this.remove();});
-        };
-    r.barchart(200, 20, 200, 100, [[55, 20, 13, 32, 5, 1, 2, 10]]).hover(fin, fout);
-    r.barchart(200, 140, 200, 100, [[32, 5, 1, 2, 10, 55, 20, 13, 55, 20, 13, 55, 20, 13]]).hover(fin, fout);
+        var fin = function () {
+                this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
+            },
+            fout = function () {
+                this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+            };
+        r.barchart(200, 20, 200, 100, [[55, 20, 13, 32, 5, 1, 2, 10]]).hover(fin, fout);
+        r.barchart(200, 140, 200, 100, [[32, 5, 1, 2, 10, 55, 20, 13, 55, 20, 13, 55, 20, 13]]).hover(fin, fout);
+    }
 
-    var r2 = Raphael('charts', 100, 30);
+    if($("#charts").length)
+    {
+        var r2 = Raphael('charts', 100, 30);
 
-    r2.barchart(0, 0, 100, 30, [[10, 20, 30, 0, 20, 50]]);
+        r2.barchart(0, 0, 100, 30, [[10, 20, 30, 0, 20, 50]]);
+    }
 });
