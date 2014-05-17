b84b0d4e7007cb1c2a5b8db372f734099ab32816
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-17 22:30:57 +0400

    add metrics

--- static/js/common.js
+++ static/js/common.js
@@ -46,12 +46,14 @@ $(function(){
     if($("#metrics-chart-1").length)
     {
         var r0 = Raphael('metrics-chart-1', 100, 100);
-//        var r1 = Raphael('spec-chart-2', 100, 50);
-//        var r2 = Raphael('spec-chart-3', 100, 50);
+        var r1 = Raphael('metrics-chart-2', 100, 100);
+        var r2 = Raphael('metrics-chart-3', 100, 100);
+        var r3 = Raphael('metrics-chart-4', 100, 100);
 
         r0.barchart(0, 0, 100, 100, [[50, 20, 30, 2, 20, 50]]);
-//        r1.barchart(0, 0, 100, 70, [[10, 20, 10, 2, 20, 50]]);
-//        r2.barchart(0, 0, 100, 70, [[40, 20, 40, 49, 20, 50]]);
+        r1.barchart(0, 0, 100, 100, [[10, 20, 10, 2, 20, 50]]);
+        r2.barchart(0, 0, 100, 100, [[40, 20, 40, 49, 20, 50]]);
+        r3.barchart(0, 0, 100, 100, [[50, 20, 30, 2, 20, 50]]);
     }
 
     $(".steps-done-item a").click(function(){
