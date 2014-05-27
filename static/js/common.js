eb0f49fa95119faff1f241c1481c8623d4b76a3a
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-28 00:36:20 +0400

    fix new theme

--- static/js/common.js
+++ static/js/common.js
@@ -1,10 +1,10 @@
 $(function(){
     $(".definition-level a").click(function(){
+        var _def_lvl = $(this).parents('.definition-level').eq(0);
+        _def_lvl.find('a').removeClass('done');
         $(this).toggleClass('done');
         var lvl = parseInt($(this).attr('rel'));
         if ($(this).hasClass('done') && lvl > 0){
-            var _def_lvl = $(this).parents('.definition-level').eq(0);
-            _def_lvl.find('a').removeClass('done');
             _.each(_.range(0, lvl+1), function(a){
                 _def_lvl.find('a[rel="' + a + '"]').addClass('done');
             })
