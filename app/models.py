dd2e23a8e1ff4fc7332f84e3d2cd04976f3c8aa5
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-28 16:16:34 +0400

    fix new theme

--- app/models.py
+++ app/models.py
@@ -9,6 +9,32 @@ from sorl.thumbnail import get_thumbnail
 from django.contrib.auth.models import User
 # Create your models here.
 
+class SummaryGroup(models.Model):
+    name = models.CharField(verbose_name=u'Name', max_length=100)
+    order = models.IntegerField(verbose_name=u'Order', default=0)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u'SummaryGroup'
+        verbose_name_plural = u'SummaryGroups'
+
+
+class SummaryItem(models.Model):
+    group = models.ForeignKey(SummaryGroup, verbose_name=u'Group')
+    name = models.CharField(verbose_name=u'Name', max_length=100)
+    text = models.TextField(verbose_name=u'Data')
+    public = models.BooleanField(default=False, verbose_name=u'Is public data')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u'SummaryItem'
+        verbose_name_plural = u'SummaryItems'
+
+
 class NewsGroup(models.Model):
     name = models.CharField(max_length=100, verbose_name=u'Название группы')
     users = models.ManyToManyField(User, verbose_name=u'Пользователи', related_name='news_groups')
