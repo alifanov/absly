285d279cc4d9298212d680edb4b2a50cce0504be
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-28 16:17:24 +0400

    fix new theme

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from app.models import News, NewsGroup
+from app.models import News, NewsGroup, SummaryGroup, SummaryItem
 
 admin.site.register(News)
 admin.site.register(NewsGroup)
+admin.site.register(SummaryGroup)
+admin.site.register(SummaryItem)
