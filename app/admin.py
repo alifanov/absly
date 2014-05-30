480e22e4f2afd0eecfca1fe7634cd149a8ac7a52
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-05-30 19:56:19 +0400

    add canvas item blocks

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from app.models import News, NewsGroup, SummaryGroup, SummaryItem, CanvasBlock,CanvasBlockItem, CanvasBlockItemParameter,\
 CanvasBlockItemParameterValue
 
+class CanvasBlockAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
 class CanvasBlockItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
@@ -9,7 +12,7 @@ admin.site.register(News)
 admin.site.register(NewsGroup)
 admin.site.register(SummaryGroup)
 admin.site.register(SummaryItem)
-admin.site.register(CanvasBlock)
+admin.site.register(CanvasBlock, CanvasBlockAdmin)
 admin.site.register(CanvasBlockItem, CanvasBlockItem)
 admin.site.register(CanvasBlockItemParameter)
 admin.site.register(CanvasBlockItemParameterValue)
