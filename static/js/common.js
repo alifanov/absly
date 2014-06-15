a65bd05ef513d0670896a80377cc019d80c43df3
vampire <lifanov.a.v@gmail.com>
2014-06-15 17:55:23 +0400

    add canvas block api

--- static/js/common.js
+++ static/js/common.js
@@ -5,7 +5,7 @@
 //        });
 //    });
 //angular.module('CanvasApp', ['CanvasAppServices']);
-function customerSegmentsCtrl($scope, Block){
+function customerSegmentsCtrl($scope){
 //    $scope.Block = $resource(
 //        '/api/canvas/:id', {id: "@id"}
 //    );
