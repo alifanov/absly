9f81a84592e6aa73041b886dd2c7bc0d4c9a2552
vampire <lifanov.a.v@gmail.com>
2014-06-15 20:20:36 +0400

    add canvas block api

--- app/admin.py
+++ app/admin.py
@@ -2,19 +2,11 @@ from django.contrib import admin
 from app.models import News, NewsGroup, SummaryGroup, SummaryItem, CanvasBlock,CanvasBlockItem, CanvasBlockItemParameter,\
 CanvasBlockItemParameterValue
 
-class CanvasBlockParameterValueInline(admin.TabularInline):
-    model = CanvasBlockItemParameterValue
-
-class CanvasBlockParameterInline(admin.TabularInline):
-    model = CanvasBlockItemParameter
-    inlines = [CanvasBlockParameterValueInline]
-
 class CanvasBlockAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
 class CanvasBlockItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
-    inlines = [CanvasBlockParameterInline]
 
 admin.site.register(News)
 admin.site.register(NewsGroup)
