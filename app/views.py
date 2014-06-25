e6215d7b9dcc7482ad2d95dba2fc8e44e2e85135
vampire <lifanov.a.v@gmail.com>
2014-06-25 21:34:38 +0400

    add GA view oauth2 test view

--- app/views.py
+++ app/views.py
@@ -57,7 +57,7 @@ class GAFunnelView(TemplateView):
             user=self.request.user
         )
         form = FunnelConfgiForm(request.POST, instance=self.funnel_config)
-        raise ValueError(form.cleaned_data['user'])
+        raise ValueError(form.user.value)
         if form.is_valid():
             form.save()
         return self.get(request, *args, **kwargs)
