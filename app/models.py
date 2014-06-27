63e21e1fd8253f0d16b611ccfb4d2c056107c3e0
vampire <lifanov.a.v@gmail.com>
2014-06-27 15:39:40 +0400

    modify ES

--- app/models.py
+++ app/models.py
@@ -148,7 +148,7 @@ class SummaryItem(models.Model):
         verbose_name_plural = u'SummaryItems'
 
 from polymorphic import PolymorphicModel
-class SummaryBlock(models.Model):
+class SummaryBlock(PolymorphicModel):
     item = models.ForeignKey(SummaryItem, verbose_name=u'Элемент Executive summary', related_name='blocks')
 
     def render(self):
