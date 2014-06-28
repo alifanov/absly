48b134f218a3e954d1054a0d5f95da2e0b5dc52c
vampire <lifanov.a.v@gmail.com>
2014-06-28 19:17:24 +0400

    save PDF summary

--- app/views.py
+++ app/views.py
@@ -830,10 +830,10 @@ class SummaryPDFView(View):
             p.setFont('Arial', 10)
             n = 0
             for i, item in enumerate(user.summary_items.order_by('pk')):
-                n += 20
+                n += 2
                 p.drawString(100, -n, item.name)
                 for ii, block in enumerate(item.blocks.all()):
-                    n += 20
+                    n += 2
                     p.drawString(120, -n, block.render_to_pdf(request))
             p.showPage()
             p.save()
