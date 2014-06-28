b9e39f880c6366410ae73394066353e8e33b5c43
vampire <lifanov.a.v@gmail.com>
2014-06-28 20:13:04 +0400

    fix image link in PDF

--- app/views.py
+++ app/views.py
@@ -823,7 +823,7 @@ def render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
     context = Context(context_dict)
     html  = template.render(context)
-    result = StringIO.StringIO()
+    result = StringIO()
     pdf = pisa.pisaDocument(StringIO.StringIO(html.encode('utf-8')), result, show_error_as_pdf=True, encoding='UTF-8')
     if not pdf.err:
         return result.getvalue()
