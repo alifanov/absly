d10c630093df6bd6a1495fff8b0a73c790e1cc01
vampire <lifanov.a.v@gmail.com>
2014-06-28 19:13:06 +0400

    save PDF summary

--- app/views.py
+++ app/views.py
@@ -826,13 +826,13 @@ class SummaryPDFView(View):
             pdfmetrics.registerFont(font_object)
             p = canvas.Canvas(response)
             p.setFont('Arial', 10)
-            i = 0
+            n = 0
             for i, item in enumerate(user.summary_items.order_by('pk')):
-                i += 20
-                p.drawString(100, i, item.name)
+                n += 20
+                p.drawString(100, n, item.name)
                 for ii, block in enumerate(item.blocks.all()):
-                    i += 20
-                    p.drawString(200, i, block.render_to_pdf(request))
+                    n += 20
+                    p.drawString(200, n, block.render_to_pdf(request))
             p.showPage()
             p.save()
             return response
