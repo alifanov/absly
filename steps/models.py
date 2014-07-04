4b5ed9e1c9dbf7b573dec42c562ca16da613fb8d
vampire <lifanov.a.v@gmail.com>
2014-07-04 16:00:45 +0400

    add steps

--- steps/models.py
+++ steps/models.py
@@ -23,7 +23,7 @@ class Step(models.Model):
     def get_deadline_last(self):
         if self.deadline is not None:
             return (self.deadline - datetime.now()).days
-        return self.deadline - datetime.now()
+        return u''
 
     def is_clear_deadline(self):
         if self.deadline and self.get_deadline_last() < 7:
