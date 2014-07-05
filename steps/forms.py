c653de43ff748b34df7bab78fa54b8e6c504668d
vampire <lifanov.a.v@gmail.com>
2014-07-05 15:49:16 +0400

    add step form modal

--- steps/forms.py
+++ steps/forms.py
@@ -3,10 +3,10 @@ from steps.models import Step
 
 class StepForm(forms.ModelForm):
 
-    def clean_deadline(self):
-        date = self.cleaned_data.get('deadline')
-        return date + ' 00:00'
-
+    # def clean_deadline(self):
+    #     date = self.cleaned_data.get('deadline')
+    #     return date + ' 00:00'
+    #
     class Meta:
         model = Step
         exclude = ('user',)
