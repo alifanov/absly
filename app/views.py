6814589c081947d17420a6a50c80edc3377de041
vampire <lifanov.a.v@gmail.com>
2014-07-09 21:42:28 +0400

    add project page

--- app/views.py
+++ app/views.py
@@ -51,7 +51,7 @@ class CreateProjectView(TemplateView):
         ctx = super(CreateProjectView, self).get_context_data(**kwargs)
         ctx['project_form'] = ProjectForm()
         ctx['project_customers_formset'] = forms.models.modelformset_factory(Project, max_num=5, extra=4,
-             widgets={'type': forms.Select())
+             widgets={'type': forms.Select()})
         return ctx
 
 class StatisticsMixin(object):
