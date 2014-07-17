b3f11da3aeac4ef07c0dee7e888c1e56b7e1f39d
vampire <lifanov.a.v@gmail.com>
2014-07-17 16:39:13 +0400

    hide discussion

--- static/js/common.js
+++ static/js/common.js
@@ -125,18 +125,25 @@ $(function(){
         });
         return false;
     });
+
+    $(document).on('click', '.done-log-btn', function(){
+        if($("#id_done_log").val() != ''){
+            $.ajax({
+                type: "GET",
+                url: "/steps/done/"+$(this).val()+"/",
+                success: function(){
+                    $("#edit-recomendation-modal-id").modal('hide');
+                    location.reload();
+                }
+            });
+        }
+        return false;
+    });
+
     $(document).on('click', '.step-done', function(){
         $(".step-edit-form .modal-footer").hide();
         $(".step-edit-form .modal-body .row").slideUp();
         $(".step-edit-form .modal-body .step-done-log").slideDown();
-//        $.ajax({
-//            type: "GET",
-//            url: "/steps/done/"+$(this).val()+"/",
-//            success: function(){
-//                $("#edit-recomendation-modal-id").modal('hide');
-//                location.reload();
-//            }
-//        });
         return false;
     });
     $(document).on('click', '.step-save', function(){
