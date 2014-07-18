601e6ea68508731903364f99383fbb1396d0baa8
vampire <lifanov.a.v@gmail.com>
2014-07-18 13:46:54 +0400

    add comments to system notifications

--- app/models.py
+++ app/models.py
@@ -158,11 +158,13 @@ class CanvasBlock(models.Model):
         return self.name
 
     def get_certainly_level(self, user):
-        els = 0.0
         M = self.elements.filter(user=user).count()
         N = 3
-        lvl = sum([int(el.level) for el in self.elements.filter(user=user)])*100.0/M/N
-        return lvl
+        if M:
+            lvl = sum([int(el.level) for el in self.elements.filter(user=user)])*100.0/M/N
+            return lvl
+        else:
+            return 0.0
 
     def is_segment(self):
         return self.name == u'Customer Segments'
