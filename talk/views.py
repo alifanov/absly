22bea6d777e29be419353cd2968824396bb43499
vampire <lifanov.a.v@gmail.com>
2014-07-18 11:20:00 +0400

    fix tooltip

--- talk/views.py
+++ talk/views.py
@@ -2,3 +2,8 @@ from django.views.generic import TemplateView
 from app.views import StatisticsMixin
 class InvestorsRequestsView(StatisticsMixin, TemplateView):
     template_name = 'investors-requests.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(InvestorsRequestsView, self).get_context_data(**kwargs)
+        ctx['active'] = 'events'
+        return ctx
