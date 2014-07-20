afe5d8d4eeeba320fb7f5b0d1e9d48e0690f1df8
vampire <lifanov.a.v@gmail.com>
2014-07-20 20:58:20 +0400

    fix bugs

--- static/js/common.js
+++ static/js/common.js
@@ -106,7 +106,7 @@ $(function(){
                 $("#edit-recomendation-modal-id .modal-content").html(resp.data);
                 $("#id_deadline").datepicker({
                     format: 'yyyy-mm-dd',
-                    orientation: 'top auto',
+                    orientation: 'auto',
                     language: 'ru'
                 });
                 $("#edit-recomendation-modal-id").modal('show');
