af3e2e92f2be6e37fc75cb55e42c18bbc7f21359
vampire <lifanov.a.v@gmail.com>
2014-07-21 19:01:46 +0400

    fix bugs

--- static/js/common.js
+++ static/js/common.js
@@ -83,7 +83,7 @@ $(function(){
 //        $(this).parents().find('.create-customer-group:visible').next().show();
 //        $(this).hide();
 //        $(this).parent().find('.del-customer-btn').show();
-        var item_last = $(this).parents().find('.create-customer-group:visible:last-child');
+        var item_last = $(this).parents().find('.create-customer-group:visible:last');
         item_last.find('.del-customer-btn').hide();
         item_last.find('.create-customer-btn').show();
         return false;
