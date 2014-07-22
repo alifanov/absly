34c87b68e0205375bc76f58f4848271698188a6d
vampire <lifanov.a.v@gmail.com>
2014-07-22 16:51:39 +0400

    fix bugs

--- app/views.py
+++ app/views.py
@@ -1204,6 +1204,7 @@ class PersonalDataView(LeftMenuMixin, FormView):
         self.request.user.set_password(form.cleaned_data['new_password1'])
         self.request.user.save()
         self.saved = True
+        raise ValueError('Saved')
         return super(PersonalDataView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
