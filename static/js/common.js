8e0dcb231cb984066e1183995dd0314ad9c67db3
vampire <lifanov.a.v@gmail.com>
2014-07-25 16:46:42 +0400

    add order number

--- static/js/common.js
+++ static/js/common.js
@@ -51,10 +51,7 @@ $(function(){
         if($(this).parent().attr('id') != 'es-guide'){
             $(this).parent().hide();
             $($(this).attr('href')).show();
-            $($(this).attr('href') + '-area a').css({
-                'position': 'relative',
-                'z-index': '101'
-            });
+            $($(this).attr('href') + '-area a').addClass('guide-area-selected');
         }
         else{
             $(".guide-modal").modal().close();
