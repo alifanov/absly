31c3e57276aeaa1a504e8919ab9fbc3b64b32b2f
vampire <lifanov.a.v@gmail.com>
2014-07-30 20:48:23 +0400

    fix modal

--- static/js/common.js
+++ static/js/common.js
@@ -43,7 +43,7 @@ function resort_steps(){
 
 $(function(){
     $("#show-guide").click(function(){
-        $(".guide-wrapper").modal().open({
+        $(".guide-wrapper").modal({
             show: true,
             backdrop: 'static',
             keyboard: false
