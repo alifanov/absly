fd2408d796a3c54a8c7b1222b9776ede12380bed
vampire <lifanov.a.v@gmail.com>
2014-08-01 17:48:44 +0400

    add revenue import

--- absly/settings.py
+++ absly/settings.py
@@ -21,6 +21,7 @@ DATABASES = {
         'PASSWORD': 'abslypass',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
+        'OPTIONS'  : { 'init_command' : 'SET storage_engine=MyISAM', },
     }
 }
 TEMPLATE_CONTEXT_PROCESSORS = (
